0e674e1055bcb396059bcee58ae8f657803f350a
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2021-09-19 10:57:37 +0300

    add config

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,11 @@ version '1.0.0'
 
 ui_page 'html/index.html'
 
-shared_script '@qb-core/import.lua'
+shared_scripts {
+    'config.lua',
+	'@qb-core/import.lua',
+}
+
 client_script 'client/main.lua'
 server_script 'server/main.lua'
 
