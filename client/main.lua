6d722d5a1697fa79c49da89c84f8bb73dfdc3d39
unknown <75336401+PedroTeixeira-unP@users.noreply.github.com>
2022-10-26 16:46:25 +0100

    Add country list input feature

--- client/main.lua
+++ client/main.lua
@@ -46,6 +46,7 @@ local function openCharMenu(bool)
         SetNuiFocus(bool, bool)
         SendNUIMessage({
             action = "ui",
+            customNationality = Config.customNationality,
             toggle = bool,
             nChar = result,
             enableDeleteButton = Config.EnableDeleteButton,
