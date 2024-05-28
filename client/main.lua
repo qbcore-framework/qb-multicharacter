70f98c39a9f02adbfc79bc914e3fefde14956b8b
SuperDaniel <56658457+TransitNode@users.noreply.github.com>
2024-05-29 01:02:23 +0200

    /logout bug fix
    Fix character spawning issue when Config.SkipSelection is true

--- client/main.lua
+++ client/main.lua
@@ -137,34 +137,43 @@ RegisterNetEvent('qb-multicharacter:client:chooseChar', function()
 end)
 
 RegisterNetEvent('qb-multicharacter:client:spawnLastLocation', function(coords, cData)
-    QBCore.Functions.TriggerCallback('apartments:GetOwnedApartment', function(result)
-        if result then
-            TriggerEvent("apartments:client:SetHomeBlip", result.type)
-            local ped = PlayerPedId()
-            SetEntityCoords(ped, coords.x, coords.y, coords.z)
-            SetEntityHeading(ped, coords.w)
-            FreezeEntityPosition(ped, false)
-            SetEntityVisible(ped, true)
-            local PlayerData = QBCore.Functions.GetPlayerData()
-            local insideMeta = PlayerData.metadata["inside"]
-            DoScreenFadeOut(500)
+    local ped = PlayerPedId()
+    SetEntityCoords(ped, coords.x, coords.y, coords.z)
+    SetEntityHeading(ped, coords.w)
+    FreezeEntityPosition(ped, false)
+    SetEntityVisible(ped, true)
 
+    QBCore.Functions.TriggerCallback('apartments:GetOwnedApartment', function(result)
+        local PlayerData = QBCore.Functions.GetPlayerData()
+        local insideMeta = PlayerData.metadata["inside"]
+
+        DoScreenFadeOut(500)
+
+        local function isInsideProperty()
+            return insideMeta.house or (insideMeta.apartment.apartmentType and insideMeta.apartment.apartmentId)
+        end
+
+        local function handleLocation()
             if insideMeta.house then
                 TriggerEvent('qb-houses:client:LastLocationHouse', insideMeta.house)
             elseif insideMeta.apartment.apartmentType and insideMeta.apartment.apartmentId then
                 TriggerEvent('qb-apartments:client:LastLocationHouse', insideMeta.apartment.apartmentType, insideMeta.apartment.apartmentId)
-            else
-                SetEntityCoords(ped, coords.x, coords.y, coords.z)
-                SetEntityHeading(ped, coords.w)
-                FreezeEntityPosition(ped, false)
-                SetEntityVisible(ped, true)
             end
-
-            TriggerServerEvent('QBCore:Server:OnPlayerLoaded')
-            TriggerEvent('QBCore:Client:OnPlayerLoaded')
-            Wait(2000)
-            DoScreenFadeIn(250)
         end
+
+        if result then
+            TriggerEvent("apartments:client:SetHomeBlip", result.type)
+        end
+
+        if isInsideProperty() then
+            handleLocation()
+        end
+
+        TriggerServerEvent('QBCore:Server:OnPlayerLoaded')
+        TriggerEvent('QBCore:Client:OnPlayerLoaded')
+
+        Wait(2000)
+        DoScreenFadeIn(250)
     end, cData.citizenid)
 end)
 
